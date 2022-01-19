2c37a1c31487b0b553cc7ae97b05b1c6fa2164f3
tanulo <tanulo@barossgabor.local>
2022-01-19 12:32:27 +0100

    2.v22

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Profile from "./components/profile.component";
 import BoardUser from "./components/board-user.component";
 import BoardModerator from "./components/board-moderator.component";
 import BoardAdmin from "./components/board-admin.component";
+import Proba from ".sajatosztalyok/Proba"
 
 class App extends Component {
   constructor(props) {
@@ -54,6 +55,12 @@ class App extends Component {
             <li className="nav-item">
               <Link to={"/home"} className="nav-link">
                 Home
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link to={"/Proba"} className="nav-link">
+                Próba
               </Link>
             </li>
 
@@ -121,6 +128,7 @@ class App extends Component {
             <Route path="/user" component={BoardUser} />
             <Route path="/mod" component={BoardModerator} />
             <Route path="/admin" component={BoardAdmin} />
+            <Route path="/Proba" component={Proba}/>
           </Switch>
         </div>
       </div>
